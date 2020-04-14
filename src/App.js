0b5861a1b3c1dc932686f7d67eab0c76a3d12627
Daniel Sottile <danielsottile@gmail.com>
2020-04-14 14:10:00 -0400

    main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import FolderList from "./components/FolderList";
-import NoteList from './components/NoteList';
 import NotFound from './components/NotFound';
+import MainPage from './components/MainPage';
 import {Route, Switch} from 'react-router-dom';
 import STORE from './dummy-store';
-import './App.css';
 
 
 
@@ -19,18 +17,14 @@ export default class App extends Component {
     return (
       <div>
         <h1>Noteful</h1>
-        <main>
-        <section className="folderList">
-        <FolderList 
-        folders={this.state.folders}
-        />
-        </section>
-        <section className="noteList ">
-        <NoteList 
-        notes={this.state.notes}
-      />
-      </section>
-      </main>
+        <Switch>
+          <Route exact path='/' render={() =>
+             <MainPage
+             folders={this.state.folders}
+             notes={this.state.notes}
+            />} />
+          <Route component={NotFound} />
+        </Switch>
     </div>
     )
   }
---
+++ src/components/MainPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import FolderList from "./FolderList";
+import NoteList from './NoteList';
+import '../App.css';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <main>
+        <section className="folderList">
+        <FolderList 
+        folders={this.props.folders}
+        />
+        </section>
+        <section className="noteList ">
+        <NoteList 
+        notes={this.props.notes}
+        />
+        </section>
+      </main>
+    )
+  }
+}
+
