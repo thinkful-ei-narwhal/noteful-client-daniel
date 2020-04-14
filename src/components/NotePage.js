a8ff855d35065a95ce6633067d9c336923e6ae9a
Daniel Sottile <danielsottile@gmail.com>
2020-04-14 15:40:38 -0400

    need to add back button

--- src/components/NotePage.js
+++ src/components/NotePage.js
@@ -8,18 +8,18 @@ export default class NotePage extends Component {
 
     
 
-    const shownNote= this.props.notes.find(note => note.id === this.props.match.params.noteId);
+    const shownNote = this.props.notes.find(note => note.id === this.props.match.params.noteId);
+
+    const shownFolder = this.props.folders.find(folder => folder.id === shownNote.folderId);
     
     console.log(shownNote);
     
      return (
        <main>
          <section className="folderList">
-         <FolderList 
-         folders={this.props.folders}
-         />
+         <h2>{shownFolder.name}</h2>
          </section>
-         <section className="noteList ">
+         <section className="noteList">
          <NoteList 
          notes={[shownNote]}
          />
