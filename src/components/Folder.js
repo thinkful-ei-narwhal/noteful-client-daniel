cbd852d98ac282f0c5ff997b45026455b0ece6bc
Cesar Morales <cesarlenin2@live.com>
2020-04-14 14:12:36 -0700

    fix anchor style

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -5,9 +5,7 @@ import "./Folder.css"
 export default class Folder extends Component {
   render() {
     return (
-      <button key={this.props.id} className="buttonFolder">
-        <NavLink to= {`/FolderLists/${this.props.id}`}><h3>{this.props.name}</h3></NavLink>
-      </button>
+        <NavLink className= "link" to= {`/FolderLists/${this.props.id}`}><h3>{this.props.name}</h3></NavLink>
     )
   }
 }
