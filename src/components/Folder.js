93ea1552e3c5e8a6842bf4c29049ec2c66dce16b
Cesar Morales <cesarlenin2@live.com>
2020-04-14 13:21:08 -0700

    turn text  blue when click

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import "./Folder.css"
 
 export default class Folder extends Component {
   render() {
     return (
       <button key={this.props.id} className="buttonFolder">
-        <Link to= {`/FolderLists/${this.props.id}`}><h3>{this.props.name}</h3></Link>
+        <NavLink to= {`/FolderLists/${this.props.id}`}><h3>{this.props.name}</h3></NavLink>
       </button>
     )
   }
