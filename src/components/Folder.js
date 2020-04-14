7899d30c2a907446fce96ffecf2dab79bf301345
Cesar Morales <cesarlenin2@live.com>
2020-04-14 11:17:31 -0700

    added folder link

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -5,7 +5,7 @@ export default class Folder extends Component {
   render() {
     return (
       <button key={this.props.id} className="buttonFolder">
-        <h3>{this.props.name}</h3>
+        <a href= {`/FolderLists/${this.props.id}`}><h3>{this.props.name}</h3></a>
       </button>
     )
   }
