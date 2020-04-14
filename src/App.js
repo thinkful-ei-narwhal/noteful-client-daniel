19eb27a7721b54829bbe5189975c23555f2af66b
Cesar Morales <cesarlenin2@live.com>
2020-04-14 13:09:11 -0700

    added link to header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NotFound from './components/NotFound';
 import MainPage from './components/MainPage';
 import FolderPage from './components/FolderPage';
 import NotePage from './components/NotePage';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import STORE from './dummy-store';
 
 
@@ -18,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Noteful</h1>
+        <Link to="/"><h1>Noteful</h1></Link>
         <Switch>
           <Route exact path='/' render={() =>
              <MainPage
